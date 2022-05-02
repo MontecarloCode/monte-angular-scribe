4944411c54cfab5822dab00da3a09d5dd0c6a3a7
sunriver <neymarjohn215@gmail.com>
2022-05-02 16:47:16 +0300

    initial project

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBFG5XAYniXLE0oysz9m2KGKudkWWUuVFo',
+    authDomain: 'scribe-angular.firebaseapp.com',
+    databaseURL: 'https://scribe-angular.firebaseio.com',
+    projectId: 'scribe-angular',
+    storageBucket: 'gs://scribe-angular.appspot.com',
+    messagingSenderId: '1029376142564'
+  }
+};
